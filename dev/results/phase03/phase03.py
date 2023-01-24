39b0d80e0813d7929a7ccccbd73ec489cf127e05
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-01-24 13:20:52 -0500

    renamed to cnwi

--- dev/results/phase03/phase03.py
+++ dev/results/phase03/phase03.py
@@ -1,11 +1,16 @@
 import os
 import sys
 
-
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+os.chdir(CURRENT_DIR)
+
+sys.path.insert(0, "../../")
+
+import code.cnwi_pipelines as cpl
 
 def main() -> None:
     return None
+
 
 if __name__ == "__main__":
     main()
