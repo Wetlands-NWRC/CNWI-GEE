a897db2562ea96edbcefa5868247e24689781c47
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-01-24 12:00:16 -0500

    updates

--- dev/code/pipelines.py
+++ dev/code/pipelines.py
@@ -1,9 +1,13 @@
 import os
 
+from typing import Dict, Any
+
 import ee
 import eelib
+import yaml
+
 from dotenv import load_dotenv
-from pipelines import eerfpl, factories, colors
+from pipelines import eerfpl, factories
 
 load_dotenv()
 
@@ -11,8 +15,51 @@ load_dotenv()
 def datacube():
     pass
 
-def benchmark():
-    pass
+def benchmark(cfg_filename: str):
+    with open(cfg_filename) as stream:
+            CFG: Dict[str, Any] = yaml.safe_load(stream=stream)
+
+    cfg_viewport = CFG['viewport']
+    viewport = ee.FeatureCollection.from_file(
+        filename=cfg_viewport.get('file_name'),
+        driver=cfg_viewport.get('driver')
+    ).geometry()
+
+    cfg_training_data = CFG['training_data']
+    training_data = ee.FeatureCollection.from_file(
+        filename=cfg_training_data.get('file_name'),
+        driver=cfg_training_data.get('driver', None),
+        layer=cfg_training_data.get('layer', None)
+    )
+
+    s1_imgs = factories.s1_factory(
+        asset_ids=CFG['assets']['benchmark']['S1']
+    )
+
+    s2_imgs = factories.s2_factory(
+        asset_ids=CFG['assets']['benchmark']['S2']
+    )
+
+    dem = ee.Image(CFG['assets']['dem']).select('elevation')
+
+    pipeline = eerfpl.BenchmarkPipeline(
+        sar=s1_imgs,
+        optical=s2_imgs,
+        dem=dem,
+        training_data=training_data
+    )
+
+    # Export Settings
+    pipeline.bucket = os.environ.get('BUCKET')
+    pipeline.root = os.environ.get('ROOT')
+    pipeline.caseNumber = CFG['phase']
+    pipeline.region = viewport
+
+    pipeline.run()
+
+    pipeline.logging()
+
+    pipeline.exportToCloud()
 
 def datacube_lsc():
     viewport = ee.FeatureCollection.from_file(
