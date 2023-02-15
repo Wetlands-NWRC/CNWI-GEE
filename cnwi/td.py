821a10a54f58f253736515e791e3b5f4c0ddbcd6
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-02-15 11:10:45 -0500

    updated training data class
    seperated data class and sampling functionality

--- cnwi/td.py
+++ cnwi/td.py
@@ -11,19 +11,42 @@ class TrainingData:
     collection: ee.FeatureCollection
     label: str = field(default="land_cover")
     value: str = field(default='value')
-    samples: str = field(default=None, init=False, repr=False)
+    samples: ee.FeatureCollection = field(default=None, init=False, repr=False)
     
     def __post_init__(self):
         self.collection = self.collection.map(eefuncs.add_xy_property)
+        
+    @property
+    def headers(self) -> list[str]:
+        properties = self.collection.first().getInfo().get('properties')
+        return list(properties.keys())
+
+    @property    
+    def labels(self) -> dict[int, str]:
+        if self.label not in self.headers:
+            raise LookupError(f"{self.label} not in headers")
     
-    #TODO create sample config / expose sampling arguments to user
-    def generate_samples(self, stack: ee.Image) -> None:
-        self.samples = stack.sampleRegions(
-            collection = self.collection,
-            properties = None,
-            scale = 10,
-            projection = None,
-            tileScale = None,
-            geometries = False
-        )
-        return None
+        labels = self.collection.aggregate_array(self.label).distinct().sort().getInfo()
+        return {lbl: value for value, lbl in enumerate(labels, start=1)}
+
+
+def generate_samples(stack: ee.Image, training_data: TrainingData, props: list[str] = None, 
+                     scale: int = 10, projection = None, tile_scale: int = 16, geom: bool = False) -> TrainingData:
+    
+    ee_lookup = ee.Dictionary(training_data.labels)
+    
+    def insert_value(element: ee.Feature):
+        return element.set(training_data.value, ee_lookup.get(element.get(training_data.label)))
+    
+    samples = stack.sampleRegions(
+        collection = training_data.collection,
+        properties = props,
+        scale = scale,
+        projection = projection,
+        tileScale = tile_scale,
+        geometries = geom
+    ).map(insert_value)
+    
+    training_data.samples = samples
+    
+    return training_data
