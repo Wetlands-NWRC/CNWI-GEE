b68797a3385a635736126fb7f1994b72de77e23b
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-02-16 21:50:19 -0500

    updates

--- cnwi/cfg.py
+++ cnwi/cfg.py
@@ -39,25 +39,5 @@ class DataCubeCfg:
         self.dest_bands = [str(_.value) for _ in dc_bands.DataCubeBands]
 
 
-@dataclass
-class RandomForestCFG:
-    stack: stack._Stack
-    training_data: td.TrainingData
-    n_trees: int = 1000
-    var_per_split: int = None
-    min_leaf: int = 1
-    bag_fraction: float = 0.5
-    max_nodes: int = None
-    seed: int = 0
-    mode: str = field(default="CLASSIFICATION")
-    predictors: list[str] = field(default_factory=list)
-    
-    def __post_init__(self):
-        _valid_modes = ['CLASSIFICATION', 'PROBABILITY', 'REGRESSION']
-        in_mode = self.mode.upper()
-        if in_mode not in _valid_modes:
-            raise TypeError(f"{self.mode} is not a valid mode")
-        else:
-            self.mode = in_mode
-        
+
     
---
+++ cnwi/pipelines.py
@@ -0,0 +1,8 @@
+
+
+class datacube:
+    
+    def __init__(self, stack, training_data, viewport = None) -> None:
+        self.stack = stack
+        self.training_data = training_data
+        
