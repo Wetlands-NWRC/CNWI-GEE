dd3d5fcc4b5012c777040142ed24fd1d0f2f634a
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-02-21 16:18:37 -0500

    added training data pre processing steps

--- cnwi/pipelines.py
+++ cnwi/pipelines.py
@@ -3,6 +3,29 @@
 class datacube:
     
     def __init__(self, stack, training_data, viewport = None) -> None:
-        self.stack = stack
-        self.training_data = training_data
-        
+        self._stack = stack
+        self._training_data = training_data
+    
+        # add some Tuning paramaters
+        # number of trees
+        # predictors 
+        # label column
+    
+    def run(self) -> None:
+        # prep training data
+        # set land cover object
+        # if data frame convert to feature collection etc
+        # sample the stack
+        # create the rf config
+        # run random forest
+        # set output prop to RFOutput
+        return None
+    
+    def mapping(self) -> dict[str, Any]:
+        # only for jupyter notebooks
+        pass
+    
+    def export(self):
+        pass
+    
+    
