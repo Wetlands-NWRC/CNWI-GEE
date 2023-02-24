ef35114e30c7fe34993ce257575cdde8dc097334
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-02-24 12:45:07 -0500

    added CDEM object

---
+++ cnwi/datasets/dem.py
@@ -0,0 +1,8 @@
+class CDEM:
+    def __new__(cls, viewport: ee.Geometry = None) -> ee.Image:
+        instance = ee.ImageCollection("NRCan/CDEM")
+
+        if viewport is not None:
+            return instance.filterBounds(viewport).mean()
+        else:
+            return instance.mean()
