e8784f2252a0fe3950efc0c5f928c489f1693c0d
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-02-28 13:02:58 -0500

    removed LandCover enums. Added Dataclasses that store Land cover and Sublandcover colors updated CMAP to be compatable with _Landcovers base class

--- cnwi/landcovers.py
+++ cnwi/landcovers.py
@@ -2,20 +2,27 @@ from __future__ import annotations
 
 import enum
 
+from abc import ABC
+from dataclasses import dataclass
+from typing import Type
+
 import pandas as pd
 
-class LandValues(enum.Enum):
-    BOG = 1
-    FEN = 2
-    FOREST = 3
-    MARSH = 4
-    SHALLOW_WATER = 5
-    SWAMP = 6
-    UPLAND = 7
-    WATER = 8    
+
+@dataclass(frozen=True)
+class _LandCovers:
+    pass
 
 
-class LandColours(enum.Enum):
+@dataclass(frozen=True)  # this will always be the same
+class LandCovers(_LandCovers):
+    WETLAND: str = '008000'
+    NON_WETLAND: str = 'FF0000'
+    WATER: str = '0000FF'
+
+
+@dataclass(frozen=True)
+class SubLandCovers(_LandCovers):  # this needs to variable
     BOG = 'A52A2A'
     FEN = 'FFF600'
     FOREST = 'FF0000'
@@ -26,45 +33,22 @@ class LandColours(enum.Enum):
     WATER = '0000FF'
 
 
-class LandCovers:
-    def __init__(self, labels: list[str]) -> None:
-        self.labels = labels
-
-        keys = [str(_.name).upper() for _ in LandValues]
-        self.labels = [_.upper() for _ in self.labels]
-        
-        if any([key in self.labels for key in keys]) == False:
-            raise Exception("Label is not in LandValues")
-
-    @property
-    def colours(self) -> dict[str, str]:
-        c = {str(obj.name): str(obj.value) for obj in LandColours
-                if str(obj.name) in self.labels}
-        c = dict(sorted(c.items(), key=lambda item: item[0]))
-        return c 
-
-    @property
-    def palette(self) -> list[str]:
-        pallette = [
-            f'#{self.colours.get(label)}' for label in self.labels
-        ]
-        return pallette
-
-
 class CMAP(pd.DataFrame):
-        
-    def __init__(self, land_cover: LandCovers) -> None:
-        
+
+    def __init__(self, in_labels: list[str], land_cover: Type[_LandCovers]) -> None:
         labels, value, r, g, b = [], [], [], [], []
-        
-        for idx, item in enumerate(land_cover.colours.items(), start=1):
-            label, hex = item
-            rgb: tuple[int] = tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
-            r.append(rgb[0]); g.append(rgb[1]); b.append(rgb[2]); value.append(idx), labels.append(label)
-        
-        data={"value": value, "red": r, "green": g, "blue": b}
+
+        for idx, label in enumerate(in_labels, start=1):
+            hex = getattr(land_cover, label.upper())
+            rgb: tuple[int] = tuple(int(hex[i:i + 2], 16) for i in (0, 2, 4))
+            r.append(rgb[0])
+            g.append(rgb[1])
+            b.append(rgb[2])
+            value.append(idx), labels.append(label)
+
+        data = {"value": value, "red": r, "green": g, "blue": b}
         super().__init__(pd.DataFrame(data=data, index=labels))
-    
+
     def to_clr(self, filename: str):
         self.to_csv(
             path_or_buf=filename,
@@ -72,11 +56,12 @@ class CMAP(pd.DataFrame):
             header=False,
             index=False
         )
-        
 
-class AAFC(enum.Enum):
-    pass
-        
-        
-        
-    
+
+def palette(lcobj: Type[_LandCovers], labels: list[str] = None) -> list[str]:
+    hexs = [getattr(lcobj, label.upper(), None) for label in labels]
+
+    pallette = [
+        f'#{hex}' for hex in hexs if hex is not None
+    ]
+    return pallette
