ea167ea379e56289aa505ba11134299d093e9e26
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-03-01 08:29:32 -0500

    added perona malik smoothening algorithm

--- cnwi/eelib/sf.py
+++ cnwi/eelib/sf.py
@@ -26,57 +26,71 @@ class Boxcar(SpatialFilter):
         return ee.Kernel.square(radius, units, normalize, magnitude)
 
 
-def perona_malik(img: ee.Image, iter: int = 10, method: int = 2) -> ee.Image:
-    K = 3.5
-    iter = 10
-    method = 2
+def perona_malik(k:float = 3.5, iter: int = 10, method: int = 2) -> callable:
+    """translated from this example here https://mygeoblog.com/2021/01/22/perona-malik-filter/
 
-    dxW = ee.Kernel.fixed(3, 3,
-                          [
-                              [0,  0, 0],
-                              [1, -1, 0],
-                              [0, 0,  0]
-                          ])
+    Args:
+        k (float, optional): _description_. Defaults to 3.5.
+        iter (int, optional): _description_. Defaults to 10.
+        method (int, optional): _description_. Defaults to 2.
 
-    dxE = ee.Kernel.fixed(3, 3,
-                          [[0, 0, 0],
-                           [0, -1, 1],
-                           [0, 0, 0]])
+    Returns:
+        callable: _description_
+    """    
+    def perona_malik_wrapper(image: ee.Image):
+        K = k
+        iter = iter
+        method = method
+        
+        dxW = ee.Kernel.fixed(3, 3,
+                           [[ 0,  0,  0],
+                            [ 1, -1,  0],
+                            [ 0,  0,  0]]);
+  
+        dxE = ee.Kernel.fixed(3, 3,
+                                [[ 0,  0,  0],
+                                    [ 0, -1,  1],
+                                    [ 0,  0,  0]]);
+        
+        dyN = ee.Kernel.fixed(3, 3,
+                                [[ 0,  1,  0],
+                                    [ 0, -1,  0],
+                                    [ 0,  0,  0]]);
+        
+        dyS = ee.Kernel.fixed(3, 3,
+                                [[ 0,  0,  0],
+                                    [ 0, -1,  0],
+                                    [ 0,  1,  0]]);
 
-    dyN = ee.Kernel.fixed(3, 3,
-                          [[0, 1, 0],
-                           [0, -1, 0],
-                           [0, 0, 0]]);
+        lamb = 0.2;
 
-    dyS = ee.Kernel.fixed(3, 3,
-                          [[0, 0, 0],
-                           [0, -1, 0],
-                           [0, 1, 0]])
+        k1 = ee.Image(-1.0/K);
+        k2 = ee.Image(K).multiply(ee.Image(K));
 
-    lamb = 0.2 #lambda
-    k1 = ee.Image(-1.0 / K)
-    k2 = ee.Image(K).multiply(ee.Image(K))
+        for _ in range(0, iter): 
+            dI_W = img.convolve(dxW)
+            dI_E = img.convolve(dxE)
+            dI_N = img.convolve(dyN)
+            dI_S = img.convolve(dyS)
 
-    for i in range(0, iter):
-        dI_W = img.convolve(dxW)
-        dI_E = img.convolve(dxE)
-        dI_N = img.convolve(dyN)
-        dI_S = img.convolve(dyS)
-
-        match method:
-            case 1:
-                cW = dI_W.multiply(dI_W).multiply(k1).exp()
-                cE = dI_E.multiply(dI_E).multiply(k1).exp()
-                cN = dI_N.multiply(dI_N).multiply(k1).exp()
-                cS = dI_S.multiply(dI_S).multiply(k1).exp()
-                img = img.add(ee.Image(lamb).multiply(cN.multiply(dI_N).add(cS.multiply(dI_S))\
-                                                      .add(cE.multiply(dI_E)).add(cW.multiply(dI_W))))
-
-            case 2:
-                    cW = ee.Image(1.0).divide(ee.Image(1.0).add(dI_W.multiply(dI_W).divide(k2)))
-                    cE = ee.Image(1.0).divide(ee.Image(1.0).add(dI_E.multiply(dI_E).divide(k2)))
-                    cN = ee.Image(1.0).divide(ee.Image(1.0).add(dI_N.multiply(dI_N).divide(k2)))
-                    cS = ee.Image(1.0).divide(ee.Image(1.0).add(dI_S.multiply(dI_S).divide(k2)))
+            match method:
+                case 1:
+                    cW = dI_W.multiply(dI_W).multiply(k1).exp();
+                    cE = dI_E.multiply(dI_E).multiply(k1).exp();
+                    cN = dI_N.multiply(dI_N).multiply(k1).exp();
+                    cS = dI_S.multiply(dI_S).multiply(k1).exp();
+                
                     img = img.add(ee.Image(lamb).multiply(cN.multiply(dI_N).add(cS.multiply(dI_S))\
-                                                          .add(cE.multiply(dI_E)).add(cW.multiply(dI_W))))
-    return img
+                        .add(cE.multiply(dI_E)).add(cW.multiply(dI_W))))
+
+                case 2:
+                    cW = ee.Image(1.0).divide(ee.Image(1.0).add(dI_W.multiply(dI_W).divide(k2)));
+                    cE = ee.Image(1.0).divide(ee.Image(1.0).add(dI_E.multiply(dI_E).divide(k2)));
+                    cN = ee.Image(1.0).divide(ee.Image(1.0).add(dI_N.multiply(dI_N).divide(k2)));
+                    cS = ee.Image(1.0).divide(ee.Image(1.0).add(dI_S.multiply(dI_S).divide(k2)));
+                
+                    img = img.add(ee.Image(lamb).multiply(cN.multiply(dI_N).add(cS.multiply(dI_S))\
+                        .add(cE.multiply(dI_E)).add(cW.multiply(dI_W))))
+
+        return img
+    return perona_malik_wrapper
