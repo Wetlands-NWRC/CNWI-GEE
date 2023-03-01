2593a158270bf55cb336895214d3ec36321c18ac
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-03-01 08:48:22 -0500

    refactored CDEM to inherit from ee.ImageCollection, removed processing from class

--- cnwi/datasets/dem.py
+++ cnwi/datasets/dem.py
@@ -3,26 +3,9 @@ from typing import Callable
 import ee
 
 
-class CDEM:
-    def __new__(cls, viewport: ee.Geometry = None, kernel=None, size: int = 1, resample_m: str = 'bicubic') -> ee.Image:
-        def resample(element): return element.resample(resample_m)
-
-        def apply_filter(filter, size) -> Callable:
-            spf = filter(size)
-
-            def apply_filter_wrapper(element: ee.Image) -> ee.Image:
-                return element.convolve(spf)
-
-            return apply_filter_wrapper
-
-        instance = ee.ImageCollection("NRCan/CDEM")
-
-        if viewport is not None:
-            instance = instance.filterBounds(viewport)
-
-        instance = instance.map(resample)
-
-        if kernel is not None:
-            instance = instance.map(apply_filter(kernel, size))
-
-        return instance
+class CDEM(ee.ImageCollection):
+    _ASSET_ID = "NRCan/CDEM"
+    
+    def __init__(self):
+        super().__init__(self.ASSET_ID)
+    
