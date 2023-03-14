b6335c3abd7c246c4daa51c50f1ccb1fbb541368
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-03-14 14:38:20 -0400

    updated class

--- cnwi/workflows.py
+++ cnwi/workflows.py
@@ -1,62 +1,63 @@
+from typing import List
+
 import ee
 
 from . import funcs
 from . import inputs
 from . import rf
 from . import td
-from .datasets import dem
-from .eelib import sf
+from .eelib import sf, eefuncs
 
 
 class _Pipeline:
-    def __init__(self, optical, sar, training_data) -> None:
+    def __init__(self, training_data: td.TrainingData, optical: List[ee.Image] = None, sar: List[ee.Image] = None,
+                 elevation_dataset: ee.Image = None, region: ee.Geometry = None) -> None:
+        self.training_data = training_data
         self.optical = optical
         self.sar = sar
-        self.training_data = training_data
-        self.dem =  dem.NASADEM_HGT().select('elevation')
-        
-        self.label_col = 'land_cover' # need to validated the label col
+        self.elevation_dataset = elevation_dataset.select('elevation')
+        self.region = region
 
-        # TODO will need to add some other paramaters that can be altered before running
+        # pipeline settings
+        self.label_col = 'land_cover' # need to validate the label col
+        # TODO will need to add some other parameters that can be altered before running
+
+        # validate inputs
+        if elevation_dataset is not None and region is None:
+            raise Exception("Region needs to spcified")
 
     def run(self):
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-        # consturct TrainingData object
+        # construct TrainingData object
         training_data = td.TrainingData(
-            collection = self.training_data,
+            collection=self.training_data,
             label=self.label_col
         )
-        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-        # build a rectanlge based off the training data
-        listCoords = ee.Array.cat(training_data.collection.geometry().bounds().coordinates(), 1)
-        xCoords = listCoords.slice(1, 0, 1)
-        yCoords = listCoords.slice(1, 1, 2)
 
-        xMin = xCoords.reduce('min', [0]).get([0,0])
-        xMax = xCoords.reduce('max', [0]).get([0,0])
-        yMin = yCoords.reduce('min', [0]).get([0,0])
-        yMax = yCoords.reduce('max', [0]).get([0,0])
+        # #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+        # build a rectangle based off the training data
+        rectangle = eefuncs.create_rectangle(self.region) if self.elevation_dataset is not None else None
 
-        rectangle = ee.Geometry.Rectangle(xMin, yMin, xMax, yMax)
-        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+        # #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
         # build inputs
-        optical_inputs = inputs.OpticalInputs(self.optical)
-        sar_inputs = inputs.SARInputs(self.sar, sf.Boxcar(1))
-        dem_inputs = inputs.DEMInputs(self.dem, rectangle=rectangle)
-        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+        optical_inputs = inputs.OpticalInputs(self.optical) if self.optical is not None else None
+        sar_inputs = inputs.SARInputs(self.sar, sf.Boxcar(1)) if self.sar is not None else None
+        dem_inputs = inputs.DEMInputs(self.elevation_dataset, rectangle=rectangle) \
+            if self.elevation_dataset is not None else None
+        # #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
         # build stack
         image_stack = inputs.stack(
             optical_inputs=optical_inputs,
             sar_inputs=sar_inputs,
             dem_inputs=dem_inputs
         )
-        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+        # #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
         # Generate samples        
         td.generate_samples(
-            stack = image_stack,
-            training_data = training_data
+            stack=image_stack,
+            training_data=training_data
         )
-        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+        # #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# #
         # Create Random Forest Config
         rf_config = rf.RandomForestCFG(
             stack = image_stack,
