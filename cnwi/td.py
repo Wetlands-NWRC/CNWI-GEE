edeccb7823b5904c9da8460bc9e386cccb854e27
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-03-22 15:54:44 -0400

    training data module construction working as expected

--- cnwi/td.py
+++ cnwi/td.py
@@ -11,6 +11,7 @@ class TrainingData:
     label: str = field(default="land_cover")
     value: str = field(default='value')
     samples: ee.FeatureCollection = field(default=None, init=False, repr=False)
+    properties: list = field(default_factory= lambda : ['land_cover', 'value', 'POINT_X', 'POINT_Y'], init=False, repr=False)
     
     def __post_init__(self):
         
@@ -18,7 +19,18 @@ class TrainingData:
         values: ee.List = ee.List.sequence(1, labels.size())
         ee_lookup = ee.Dictionary.fromLists(labels, values)
         
-        self.collection = self.collection.map(funcs.add_xy_property).map(lambda x: x.set(self.value, ee_lookup.get(x.get(self.label))))
+        self.collection = self.collection.map(funcs.add_xy_property)\
+            .map(self._insert_values(self.label, ee_lookup))
+
+    def _insert_values(self, column, lookup) -> callable:
+        def wrapper(element: ee.Feature):
+            key = element.get(column)
+            new = element.set({
+                'land_cover': ee.String(key).toLowerCase(),
+                'value': lookup.get(key) 
+            })
+            return new.select(self.properties)
+        return wrapper
 
     def sample(self, stack: ee.Image, scale: int = 10, projection = None, tile_scale: int = 16,
                geom: bool = False) -> None:
