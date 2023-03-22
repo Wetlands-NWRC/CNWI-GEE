551a7e69486cd9a90e98f9964477aeca7f77e3c2
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-03-22 11:32:10 -0400

    refactored

--- cnwi/inputs.py
+++ cnwi/inputs.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-from abc import ABC
 from dataclasses import dataclass, field, InitVar
-from typing import Dict, List, Callable
+from typing import Dict, List, Callable, Union
 
 import ee
 import tagee
@@ -13,88 +12,44 @@ from . import derivatives as driv
 
 from .eelib import eefuncs
 
-@dataclass
-class Inputs:
-    products: list = field(default_factory=list)
+
+def s2_inputs(assets: list[str]) -> List[ee.Image]:
+    # optcial inputs 
+    s2s = [imgs.Sentinel2(_) for _ in assets]
+    ndvis = driv.batch_create_ndvi(s2s)
+    savis = driv.batch_create_savi(s2s)
+    tassels = driv.batch_create_tassel_cap(s2s)
+
+    return [*s2s, *ndvis, *savis, *tassels]
 
 
-def s2_inputs(assets: list[str]) -> Inputs:
-    pass
-
-
-def s1_inputs(assets: list[str], spatial_filter = None) -> Inputs:
+def s1_inputs(assets: list[str], s_filter = None) ->List[ee.Image]:
     # prep the inputs
-    s1s = [imgs.Sentinel1(_) for _ in s1]
+    s1s = [imgs.Sentinel1(_) for _ in assets]
     # sar inputs
-    boxcar = sfilters.boxcar(1)
-    sar_pp1 = eefuncs.batch_despeckle(s1s, boxcar)
-    
+    s_filter = sfilters.boxcar(1) if s_filter is None else s_filter
+    sar_pp1 = eefuncs.batch_despeckle(s1s, s_filter)
     # sar derivatives
     ratios = driv.batch_create_ratio(
         images=sar_pp1,
         numerator='VV',
         denominator='VH'
     )
+    return [*sar_pp1, *ratios]
 
 
-def elevation_inputs(dem: ee.Image) -> Inputs:
-    pass
-
-
-@dataclass(frozen=True)
-class OpticalInputs:
-    ee_images: InitVar[list[ee.Image]]
-    products:list[ee.Image] = field(default_factory=list)
-    def __post_init__(self, ee_images):
-        self.products.extend(ee_images)
-        self.products.extend(eefuncs.batch_create_ndvi(ee_images))
-        self.products.extend(eefuncs.batch_create_savi(ee_images))
-        self.products.extend(eefuncs.batch_create_tassel_cap(ee_images))
-
-
-@dataclass(frozen=True)
-class SARInputs:
-    ee_images: InitVar[list[ee.Image]]
-    s_filter: InitVar
-    products: list[ee.Image] = field(default_factory=list)
-    def __post_init__(self, ee_images, s_filter):
-        pp_1 = eefuncs.batch_despeckle(ee_images, s_filter)
-        self.products.extend(pp_1)
-        self.products.extend(eefuncs.batch_create_ratio(pp_1, 'VV', 'VH'))
-
-
-@dataclass(frozen=False)
-class DEMInputs:
-    ee_image: InitVar[ee.Image]
-    rectangle: InitVar[ee.Geometry]
-    s_filter: Dict[Callable, List[int]] = field(default_factory=lambda: {sfilters.gaussian_filter(3): ['Elevation', 'Slope', 'GaussianCurvature'],
-                                                                         sfilters.perona_malik(): ['HorizontalCurvature', 'VerticalCurvature',
-                                                                                             'MeanCurvature']})
-    products: list[ee.Image] = field(default_factory=list)
+def elevation_inputs(dem: ee.Image, rectangle: ee.Geometry, s_filter: Dict[Callable, List[Union[str, int]]] = None):
+    if s_filter is None:
+        s_filter = {
+            sfilters.gaussian_filter(3): ['Elevation', 'Slope', 'GaussianCurvature'],
+            sfilters.perona_malik(): ['HorizontalCurvature', 'VerticalCurvature', 'MeanCurvature']
+        }
     
-    def __post_init__(self, ee_image, rectangle):
-
-        for sfilt, selector in self.s_filter.items():
-            smoothed = sfilt(ee_image)
-            ta = tagee.terrainAnalysis(smoothed, rectangle).select(selector)
-            self.products.append(ta)
-            ta, smoothed = None, None
-
-
-@dataclass
-class Additional:
-    products: list [ee.Image] = field(default_factory=list)
-    
-    def __len__(self):
-        return len(self.products)
-    
-    def __setitem__(self, _idx, _object):
-        self.products[_idx] = _object
-    
-    def __getitem__(self, key):
-        return self.products[key]
-
-
-def stack(optical_inputs: OpticalInputs, sar_inputs: SARInputs = None, dem_inputs: DEMInputs = None):
-    return ee.Image.cat(*optical_inputs.products, *sar_inputs.products, *dem_inputs.products)
-
+    out = []
+    for filter, selector in s_filter.items():
+        smoothed = filter(dem)
+        ta = tagee.terrainAnalysis(smoothed, rectangle).select(selector)
+        out.append(ta)
+        ta, smoothed = None, None
+    return out 
+        
