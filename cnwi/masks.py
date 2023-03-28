bd7bbe25eaa1988d0bf710f78e7ff8458b8abb6c
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-03-28 10:28:27 -0400

    moved

--- cnwi/masks.py
+++
@@ -1,7 +0,0 @@
-import ee
-
-
-def slope_mask(slope: ee.Image, deg: int = 15):
-    return slope.lte(deg)
-
-    
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cnwi-gee',
+    packages=find_packages(where='cnwi')
+)
