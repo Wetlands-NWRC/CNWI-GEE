d17111d03a5c077aa6e8e67b4591a7fbbc79fd18
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-03-28 16:48:33 -0400

    updated rf model to accept mode

--- cnwi/rf.py
+++ cnwi/rf.py
@@ -13,10 +13,15 @@ class RandomForestModel:
         self.bagFraction = bagFraction
         self.maxNodes = maxNodes
         self.seed = seed
-    
+        self.mode = "CLASSIFICATION"
+        
     @property
     def classifier(self):
-        return ee.Classifier.smileRandomForest(**self.__dict__)
+        props = self.__dict__
+        return ee.Classifier.mode(props.pop('mode')).smileRandomForest(**props)
+
+    def show_properties(self) -> dict:
+        return self.__dict__
 
     def train(self, training_data: ee.FeatureCollection, predictors: List[str], classProperty: str) -> "ee.Classifier":
         """trains the random forest model
