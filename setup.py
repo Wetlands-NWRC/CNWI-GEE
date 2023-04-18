1e7f35b26f146adc60c023860392a3141a3e190c
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-04-18 10:01:22 -0400

    removed pyproject added setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
 from setuptools import setup, find_packages
 
-setup()
+setup(
+    name='cnwi_gee',
+    version='3.0.0',
+    install_requires =[
+        'earthengine-api',
+        'pandas',
+        'numpy',
+        'tagee',
+    ],
+    packages=find_packages(
+        include=['cnwi', 'cnwi.*'],
+        exclude=['examples', 'scripts', 'bin', '.vscode']
+    )
+)
