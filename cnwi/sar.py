7346bd1af4d163686bf7949d99b08ec86f145856
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-05-04 15:43:02 -0400

    added sar module

---
+++ cnwi/sar.py
@@ -0,0 +1,39 @@
+import ee
+
+from . import derivatives as d
+from . import sfilters as s
+
+
+def build_alos_inpts(target_yyyy: int) -> ee.Image:
+    mosaic: ee.Image = ee.ImageCollection("JAXA/ALOS/PALSAR/YEARLY/SAR_EPOCH").\
+        filterDate(f'{target_yyyy}', f'{target_yyyy + 1}').select('H.*').first()
+    
+    ratio = d.Ratio(
+        numerator='HH',
+        denominator='HV'
+    )
+    
+    with_ratio = ratio.calculate(mosaic)
+    return with_ratio
+
+
+def build_s1_inputs(col: ee.ImageCollection) -> ee.Image:
+    EARLY_SEASON = '04-01', '06-20'
+    LATE_SEASON = '06-21', '10-31'
+    
+    # select only VV and VH
+    in_col = col.select('V.*')
+    # apply spatial filter
+    boxcarf = s.boxcar(1)
+    s1_pp1 = in_col.map(boxcarf)
+    # add ratio
+    ratio = d.Ratio('VV', 'VH')
+    s1_pp2 = s1_pp1.map(ratio)
+    # sperate into early and late season
+    early = s1_pp2.filterDate(*EARLY_SEASON)
+    late = s1_pp2.filterDate(*LATE_SEASON)
+    # mosaic early and late
+    early_mosaic = early.mosaic()
+    late_mosaic = late.mosaic()
+    # concat early and late into one image
+    return ee.Image.concat(early_mosaic, late_mosaic)
