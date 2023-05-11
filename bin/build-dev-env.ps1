b7b3b83a3e1e5b2a71f38d8346b7a679ee15b53c
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-05-11 15:05:55 -0400

    moved to src layout from flat

--- bin/build-dev-env.ps1
+++
@@ -1,8 +0,0 @@
-conda create -n cnwi-gee-dev -c conda-forge python=3.10 geopandas pandas earthengine-api matplotlib python-dotenv geemap -y
-conda activate cnwi-gee-dev
-
-conda remove pyproj --force -y
-python -m pip install pyproj
-
-Write-Output "Installing CNWI package"
-python -m pip install -e  "../"
