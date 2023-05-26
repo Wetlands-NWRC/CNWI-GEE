a4717319353e656fc0bb3dbad6c77494e0d1392d
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-05-26 07:59:19 -0400

    reverted back to old way of doing the transform

--- src/cnwi/fourier.py
+++ src/cnwi/fourier.py
@@ -131,8 +131,9 @@ def fit(model: model) -> ee.ImageCollection:
     return model.harmonics.map(fit_wrapper)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-def fourier(target_aoi: ee.Geometry, modes: int = 3, cloud_pix_percent: int = 20,
-            variable: d._RasterCalculator = None, date_range: Tuple[str] = None) -> FourierImage:
+# TODO update function def to use raster calc object, 
+def fourier(ee_object: ee.ImageCollection, modes: int = 5, omega: float = 1.5, nir: str = None, 
+            red: str = None) -> FourierImage:
     """Applys fourier transform to defined image collection. Image Collection needs to be filtered 
     by start and end date, as well have a cloud mask applied. It also needs to have NDVI band in the 
     stack
@@ -142,37 +143,19 @@ def fourier(target_aoi: ee.Geometry, modes: int = 3, cloud_pix_percent: int = 20
         modes (int, optional): number of modes to include. Defaults to 5.
         omega (float, optional): _description_. Defaults to 1.5.
     """
-    
-    def cloud_mask(element: ee.Image):
-        qa = element.select('QA60')
-        cloudBitMask = 1 << 10
-        cirrusBitMask = 1 << 11
-        mask = qa.bitwiseAnd(cloudBitMask).eq(0)\
-            .And(qa.bitwiseAnd(cirrusBitMask).eq(0))
-        return element.updateMask(mask)
-    
-    def min_max(*dates):
-        """ Calculates the time dif in days """
-        t1, t2 = dates
-        dif = ee.Date(t1).difference(ee.Date(t2), 'day').abs()
-        return dif.gt(365)   
-    
-    DATES = ('2017', '2022') if date_range is None else date_range
-    VARIABLE = d.NDVI() if variable is None else variable
-    OMEGA = 1 if min_max(*DATES) else 1.5
-    
-    t1, t2 = DATES
     # construct the input collection
-    time_series = Sentinel2SR().filterBounds(target_aoi).filterDate(str(t1), str(t2))\
-        .filter(f'CLOUDY_PIXEL_PERCENTAGE > {cloud_pix_percent}')\
-        .map(cloud_mask)\
-        .map(VARIABLE)\
-        .map(add_constant)\
-        .map(add_time(omega=OMEGA))
+    nir = "B8" if nir is None else nir
+    red = "B4" if red is None else red
     
+    # TODO add date minMax checker, if greater than 365 days use omega of 1
+    
+    time_series = ee_object.map(add_ndvi(nir=nir, red=red))\
+        .map(add_constant)\
+        .map(add_time(omega=omega))\
+
     fourier_model = model(
         ee_object=time_series,
         modes=modes,
-        dependent_var=VARIABLE.NAME
+        dependent_var="NDVI"
     )
     return FourierImage(model=fourier_model, ee_image_collection=time_series)
